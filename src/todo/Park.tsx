95c5c042a40960d94df408b6cc9f455ac36cea84
Ichim Stefan <stefanichim2201@gmail.com>
2023-10-25 20:58:39 +0300

    Add useState() useCallback() useMemo()

--- src/todo/Park.tsx
+++ src/todo/Park.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 interface ParkProps {
   id?: string;
-  text: string;
+  description: string;
+  squared_kms: number;
+  last_review: Date;
+  reaches_eco_target: boolean;
 }
 
-const Item: React.FC<ParkProps> = ({ id, text }) => {
+const Item: React.FC<ParkProps> = ({ id, description, squared_kms, last_review, reaches_eco_target }) => {
   return (
-    <div>{text}</div>
+    <div>
+        ID = {id}<br/>
+        {description}<br/>
+        Squared kms = {squared_kms}<br/>
+        Does it reach ecological target? {reaches_eco_target ? "Yes" : "No"}<br/>
+        Last review: {last_review.getDate() + "/" + last_review.getMonth() + "/" + last_review.getFullYear()}
+    </div>
   );
 };
 
--- src/todo/ParkList.tsx
+++ src/todo/ParkList.tsx
@@ -1,8 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { add } from 'ionicons/icons';
+import React, { useCallback, useMemo, useState } from 'react';
 import Park from './Park';
+import { getLogger } from '../utils';
 
-const ItemList: React.FC = () => {
+
+const log = getLogger('ParkList');
+
+const ParkList: React.FC = () => {
+  const [parks, setParks] = useState([
+    {id:"0", description: "Park 1", squared_kms:22, last_review: new Date(Date.now()), reaches_eco_target: true},
+    {id:"1", description: "Park 2", squared_kms:30, last_review: new Date(Date.now()), reaches_eco_target: false},
+  ]);
+
+  let count = useMemo(() => {
+    log('Calculate count');
+    return parks.length;
+  }, [parks]);
+
+  const addItem = useCallback(() => {
+    const id = `${parks.length + 1}`;
+    log('addItem');
+    setParks(parks.concat({id, description: "new park", squared_kms:40, last_review: new Date(Date.now()), reaches_eco_target: false}));
+  }, [parks, setParks]);
+
+  log('render');  
   return (
     <IonPage>
       <IonHeader>
@@ -11,11 +33,22 @@ const ItemList: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <Park text="Park 1"/>
-        <Park text="Park 2"/>
+        <div>Item count: {count}</div>
+        {parks.map(({id, description, squared_kms, last_review, reaches_eco_target}) => 
+          <Park key={id} id={id} 
+            description={description} 
+            squared_kms={squared_kms} 
+            last_review={last_review} 
+            reaches_eco_target={reaches_eco_target}
+          />)}
+          <IonFab vertical='bottom' horizontal='end' slot='fixed'>
+            <IonFabButton onClick={addItem}>
+                <IonIcon icon={add}/>
+            </IonFabButton>
+          </IonFab>
       </IonContent>
     </IonPage>
   );
 };
 
-export default ItemList;
+export default ParkList;
